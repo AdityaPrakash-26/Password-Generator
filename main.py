6d579daa152023fdeb0c05d7b9159341bf35371f
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-06 18:20:09 +0530

    Added multiple password support

--- main.py
+++ main.py
@@ -14,32 +14,35 @@ special_alpha = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
                     "/", ":", ";", "<", ">", ",", ".", "~", "`"]
 
 nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+again = 'Y'
+while again!='N':
+    length = int(input("What should be the length of your password?\n"))
 
-length = int(input("What should be the length of your password?\n"))
+    choice_array = lower_alpha
 
-choice_array = lower_alpha
+    password = ""
 
-password = ""
+    print("Do you want upper-case characters in your password?(Y/N)\n")
+    upper_choice = input()
 
-print("Do you want upper-case characters in your password?(Y/N)\n")
-upper_choice = input()
+    print("Do you want special characters in your password?(Y/N)\n")
+    special_choice = input()
 
-print("Do you want special characters in your password?(Y/N)\n")
-special_choice = input()
+    print("Do you want numbers in your password?(Y/N)\n")
+    num_choice = input()
 
-print("Do you want numbers in your password?(Y/N)\n")
-num_choice = input()
+    if(upper_choice == 'Y'):
+        choice_array.extend(upper_alpha)
+    if(special_choice == 'Y'):
+        choice_array.extend(special_alpha)
+    if(num_choice == 'Y'):
+        choice_array.extend(nums)
 
-if(upper_choice == 'Y'):
-    choice_array.extend(upper_alpha)
-if(special_choice == 'Y'):
-    choice_array.extend(special_alpha)
-if(num_choice == 'Y'):
-    choice_array.extend(nums)
+    print("\nGenerating password\n")
 
-print("\nGenerating password\n")
+    for _ in range(length):
+        x = random.choice(choice_array)
+        password += x
+    print("Your password is {}\n".format(password))
 
-for _ in range(length):
-    x = random.choice(choice_array)
-    password += x
-print("Your password is {} ".format(password))
+    again = input("Would you like to generate another password?(Y/N)\n")
